45f1e271a60205ff8ea00a0d555c97116e07059b
Mohammed Boukhal <mboukhal@e1r10p2.1337.ma>
2022-09-01 11:42:33 +0100

    Thu Sep  1 11:28:03 +01 2022

--- main.cpp
+++ main.cpp
@@ -3,24 +3,27 @@
 
 int main(void)
 {
-	
-  std::string line;
-  std::string binary;
-  std::ifstream myfile ("Makefile");
-  if (myfile.is_open())
-  {
-    while (getline(myfile, line))
-	{
-		line += '\n';
-	    binary += toBinary(line);
-	}
 
-	std::cout << binary << "\n\n";
-	binary = compres(binary);
-	std::cout << binary;
-    myfile.close();
-  }
-  else std::cout << "Unable to read form file"; 
+	std::string line;
+	std::string binary;
+	char	c;
+	std::ifstream myfile ( "utils.cpp" );
+
+
+	if ( myfile.is_open() )
+	{
+		while ( myfile.get( c ) )
+			binary += c;
+
+		binary = toBinary( binary.c_str() );
+
+		std::cout << binary;
+		binary = compres( binary );
+		// std::cout << binary;
+		myfile.close();
+	}
+	else
+		std::cout << "Unable to read form file"; 
 
 	return EXIT_SUCCESS;
 }
--- utils.cpp
+++ utils.cpp
@@ -1,61 +1,60 @@
 #include "compresor.hpp"
  
-std::string toBinary(std::string const &str)
+std::string toBinary( std::string const &str )
 {
     std::string binary;
 
-	for (int i = 0; i < (int)str.size(); i++)
-        binary += std::bitset<8>(str[i]).to_string();
+	for ( int i = 0; i < (int)str.size(); i++ )
+        binary += std::bitset<8>( str[i] ).to_string();
     return binary;
 }
 
-std::string compres(std::string const &str)
+std::string compres( std::string const &str )
 {
 	int one = 0, zero = 0;
 	std::string comp;
 	comp += str[0];
 	comp += " \n";
 	float strsize = (float)str.size();
-	for (int i = 0; i < (int)strsize; i++)
-	{
-		if (str[i] == '0')
-		{
-			while (str[i++] == '0')
+
+	for ( int i = 0; i < (int)strsize; i++ ) {
+
+		if ( str[i] == '0' ) {
+
+			while ( str[i++] == '0' )
 				zero++;
-			if (zero > 9)
-			{
+			if ( zero > 9 ){
+
 				comp += '.';
+				comp += std::to_string(zero).size();
 				comp += std::to_string(zero);
-				comp += '.';
 
 			}
 			else
 				comp += zero + '0';
-				zero = 0;
-				// last = 0;
+			zero = 0;
 		}
-		else if (str[i] == '1')
-		{
-			while (str[i++] == '1')
+		else if ( str[i] == '1' ) {
+
+			while ( str[i++] == '1' )
 				one++;
-			if (one > 9)
-			{
+			if ( one > 9 ) {
+
 				comp += '.';
-				comp += std::to_string(one);
-				comp += '.';
+				comp += std::to_string( one ).size();
+				comp += std::to_string( one );
 			}
 			else
 				comp += one + '0';
 			one = 0;
 		}
-		std::cout <<'[' << i << ']'<< std::endl;
 	}
-	float compsize = (float)comp.size();
-	std::cout << "size of str: " << strsize
-			  << " size of comp: " << compsize
-			  << " comprese porsontage: " << (100 - ((compsize * 100) / strsize))
-			  << "%"
-			  << std::endl; 
+
+	float compsize = (float)comp.size(), porsontage = (100 - ((compsize * 100) / strsize));
+
+	std::cout << "\n\t\tsize of str: \t\t" << strsize
+			  << "\n\t\tsize of comp: \t\t" << compsize;
+	printf( "\n\t\tcomprese porsontage: \t%.2lf%%\n", porsontage );
 	return comp;
 }
 
